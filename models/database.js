01c80667f7439b06ad616f9639e8864402c51434
Luke Brown <lukebrown4290@hotmail.com>
2015-10-18 17:45:33 +0000

    added databases.js

---
+++ models/database.js
@@ -0,0 +1,7 @@
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL;
+
+var client = new pg.Client(connectionString);
+client.connect();
+var query = client.query('CREATE TABLE leaderboard(id SERIAL PRIMARY KEY, name VARCHAR(40) not null, score NUMERIC(10)');
+query.on('end', function() { client.end(); });
