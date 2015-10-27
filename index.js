24fc0d8fbc2031414ee7cffbbfe2920e760f9777
Luke Brown <lukebrown4290@hotmail.com>
2015-10-27 22:43:04 +0000

    added simple post request

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var bodyParser = require('body-parser');
+var pg = require('pg');
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({extended: true})); 
+
+app.get('/', function(request, response){
+  response.send('<h1>Game Services</h1>');
+});
+
+app.post('/', function(request, response)
+{
+  var score = request.body.score;
+  console.dir(score);
+  
+  response.writeHead(200, {'Content-Type': 'application/json'});
+  response.end(JSON.stringify({score : score}));
 });
 
 http.listen(process.env.PORT, function(){
